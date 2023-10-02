93621870838792427ff12f7468e19d675b66e3b1
MaiteNavas <maitenavas626@gmail.com>
2023-10-02 19:19:49 +0200

    Contact form ok

--- PP_HomeRoom_Front/src/pages/Contact.jsx
+++ PP_HomeRoom_Front/src/pages/Contact.jsx
@@ -1,8 +1,24 @@
 import {Link } from 'react-router-dom';
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 
 function Contact(){
+	const form = useRef();
+	const [isSent, setIsSent] = useState(false); 
 
+	const sendEmail = (e) => {
+	  e.preventDefault();
+  
+	  emailjs.sendForm('service_sotu7bi', 'template_k77dyg7', form.current, 'aQxnGmooVdSB5QoGS')
+		.then((result) => {
+			console.log(result.text);
+			form.current.reset();
+			setIsSent(true); 
+		}, (error) => {
+			console.log(error.text);
+		});
+	};
 
     return(
         <>
@@ -15,10 +31,13 @@ function Contact(){
 						<h1 className="text-xl font-semi-bold leading-tight tracking-tight text-[#213555] md:text-2xl dark:text-white">
 							Contacta con nosotros
 						</h1>
+						{isSent ? (
+                		<div className="text-green-500">¡El formulario se ha enviado con éxito!</div>
+              			) : (
 						<form
 							className="space-y-4 md:space-y-6"
-							action="#"
-							method="post"
+							ref={form} 
+							onSubmit={sendEmail}
 							>
 							<div>
 								<label
@@ -28,7 +47,7 @@ function Contact(){
 								</label>
 								<input
 									type="text"
-									name="name"
+									name="user_name"
 									id="name"
 									className="bg-gray-50 border border-gray-300 text-[#213555] sm:text-sm rounded-lg focus:ring-[#213555] focus:border-[#213555] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									
@@ -44,7 +63,7 @@ function Contact(){
 								</label>
 								<input
 									type="email"
-									name="email"
+									name="user_email"
 									id="email"
 									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#213555] focus:border-[#213555] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									
@@ -52,30 +71,32 @@ function Contact(){
 								/>
 
 							</div>
-                            <div>
+							<div>
 								<label
-									htmlFor="cpassword"
-									className="block mb-1 text-sm font-medium text-gray-900 dark:text-white">
+									htmlFor="user_phone"
+									className="block mb-1 text-sm font-medium text-[#213555] dark:text-white">
 									Teléfono
 								</label>
 								<input
 									type="text"
-									name="cpassword"
-									id="cpassword"
+									name="user_phone"
+									id="user_phone"
 									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#213555] focus:border-[#213555] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+									
 									required
 								/>
+
 							</div>
 							<div>
 								<label
-									htmlFor="password"
+									htmlFor="comment"
 									className="block mb-1 text-sm font-medium text-[#213555] dark:text-white">
 									Comentario
 								</label>
 								<textarea
 									type="text"
-									name="password"
-									id="password"
+									name="message"
+									id="comment"
 									placeholder="Escribe tu comertario..."
 									className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#213555] focus:border-[#213555] block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
 									required
@@ -91,6 +112,7 @@ function Contact(){
 							</button>
 							
 						</form>
+						)}
 					</div>
 				</div>
 			</div>
